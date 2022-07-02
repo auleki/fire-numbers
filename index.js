eee49642daee1d0e8e09ed172435b204736b6681
Emmanuel B. Giwa <kodagiwa@gmail.com>
2022-07-02 18:13:31 +0100

    update index file with exports

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Nothing here for now. Other files have been moved
+const addCommas = require('./addCommas');
+const removeCommas = require('./removeCommas');
+
+module.exports = {
+  addCommas,
+  removeCommas
+};
